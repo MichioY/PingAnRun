048224372b8424a442bd910dc23c05b5c0cffa14
袁健强 <opooho@qq.com>
2017-08-08 19:13:40 +0800

    添加修改功能

---
+++ WeChatEnvTestViewController/WeChatEnvTestViewController/KKPARunConfig.h
@@ -0,0 +1,13 @@
+//
+//  KKPARunConfig.h
+//  WeChatEnvTestViewController
+//
+//  Created by SuperDry on 08/08/2017.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KKPARunConfig : NSObject
+
+@end
---
+++ WeChatEnvTestViewController/WeChatEnvTestViewController/KKPARunConfig.m
@@ -0,0 +1,13 @@
+//
+//  KKPARunConfig.m
+//  WeChatEnvTestViewController
+//
+//  Created by SuperDry on 08/08/2017.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+#import "KKPARunConfig.h"
+
+@implementation KKPARunConfig
+
+@end
